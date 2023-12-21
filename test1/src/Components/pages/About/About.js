8fa590d49c8aaeed19aa1c56e3af14d0b0ce45f7
ghayth31 <gharsallighaith31@gmail.com>
2023-12-21 14:25:34 +0100

    pages

---
+++ test1/src/Components/pages/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function about() {
+  return (
+    <div>
+      we are here to help you
+    </div>
+  )
+}
+
+export default about
--- test1/src/Components/pages/Home/Home.js
+++ test1/src/Components/pages/Home/Home.js
@@ -1,17 +1,11 @@
 import React from 'react'
 
 function Home() {
-
-
-
   return (
     <div>
-        <h1>Home</h1>
+      heeey
     </div>
   )
 }
-
-
-
 
 export default Home
