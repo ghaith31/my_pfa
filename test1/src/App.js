a15985b319154a4312183531544ec9074cf29471
ghayth31 <gharsallighaith31@gmail.com>
2023-12-21 14:31:58 +0100

    App

--- test1/src/App.js
+++ test1/src/App.js
@@ -1,9 +1,11 @@
+//import logo from './logo.svg'; (we will fixed 2days ago friday PLD)
 import { BrowserRouter , Routes, Route } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
-import Home from './Components/Home/Home';
-import SignIn from './Components/SignIn/SignIn';
-import SinUp from './Components/S'
+import Home from './Components/pages/Home/Home';
+import SignIn from './Components/pages/SignIn/SignIn';
+import SinUp from './Components/pages/SignUp/SignUp';
+import About from './Components/pages/About/About';
+import logo from './logo.svg';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
     <Routes>
     <Route path = "/" element = {<Home/>} />
     <Route path = "/SignIn" element = {<SignIn/>} />
+    <Route path = "/SinUp" element = {<SinUp/>} />
+    <Route path = "/about" element = {<About/>} />
     
     </Routes>
     </BrowserRouter>
