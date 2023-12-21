2dfd6149ca687aa397379d179752fe1ab89d88c5
ghayth31 <gharsallighaith31@gmail.com>
2023-12-21 14:03:25 +0100

    pages

--- test1/src/App.js
+++ test1/src/App.js
@@ -1,9 +1,9 @@
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home/Home';
 import SignIn from './Components/SignIn/SignIn';
-import { BrowserRouter , Routes, Route } from 'react-router-dom';
-
+import SinUp from './Components/S'
 
 function App() {
   return (
