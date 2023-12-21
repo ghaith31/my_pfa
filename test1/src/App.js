11e79da3b71ff558d2c29439ae18192893b3a6c3
ghayth31 <gharsallighaith31@gmail.com>
2023-12-21 15:13:45 +0100

    App

--- test1/src/App.js
+++ test1/src/App.js
@@ -7,10 +7,13 @@ import SinUp from './Components/pages/SignUp/SignUp';
 import About from './Components/pages/About/About';
 import logo from './logo.svg';
 import Profile from './Components/pages/Profile/Profile';
+import Header from './Components/Header/Header';
+
 function App() {
   return (
     <div>
     <BrowserRouter >
+    <Header/>
     <Routes>
     <Route path = "/" element = {<Home/>} />
     <Route path = "/SignIn" element = {<SignIn/>} />
