87ce82b50371e0ee3eeca72e320da476f816ca1a
ghayth31 <gharsallighaith31@gmail.com>
2023-12-21 14:54:26 +0100

    App

--- test1/src/App.js
+++ test1/src/App.js
@@ -6,7 +6,7 @@ import SignIn from './Components/pages/SignIn/SignIn';
 import SinUp from './Components/pages/SignUp/SignUp';
 import About from './Components/pages/About/About';
 import logo from './logo.svg';
-
+import Profile from './Components/pages/Profile/Profile';
 function App() {
   return (
     <div>
@@ -16,7 +16,7 @@ function App() {
     <Route path = "/SignIn" element = {<SignIn/>} />
     <Route path = "/SinUp" element = {<SinUp/>} />
     <Route path = "/about" element = {<About/>} />
-    
+    <Route path = "/Profile" element = {<Profile/>} />
     </Routes>
     </BrowserRouter>
     </div>
