826b699cc0d30954d0fcbe033a7f394065ce0179
ghayth31 <gharsallighaith31@gmail.com>
2023-12-26 17:24:22 +0100

    !!!!!

--- api/index.js
+++ api/index.js
@@ -1,9 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+mongoose
+  .connect("mongodb+srv://libery:libery@libery.rh2fcjr.mongodb.net/?retryWrites=true&w=majority")
+  .then(() => {
+  console.log('MongoDB Connected');
+})
+.catch((err) => {
+  console.log(err);
+});
+
 const app = express();
 
-//msg error (ask jasem in pld)
-app.listen(3000, () => {
-    
-    console.log('Server is running on port 3000');
+app.listen(3000, () => {    
+    console.log('Server is running on port 30000');
 }    
 );
