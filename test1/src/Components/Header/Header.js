ad2058e17a0f039bf5ca5cd02811e64c03c53161
ghayth31 <gharsallighaith31@gmail.com>
2024-02-18 16:10:25 +0100

    change about page

--- test1/src/Components/Header/Header.js
+++ test1/src/Components/Header/Header.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import { Navbar } from 'flowbite-react';
-import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { AiOutlineSearch } from 'react-icons/ai';
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
+import { HiAdjustments, HiCloudDownload, HiUserCircle } from 'react-icons/hi';
+
 
 function Header() {
     const { currentUser } = useSelector((state) => state.user);
   return (
     <header className='bg-slate-200 shadow-md'>
     <div className='flex justify-between items-center max-w-7xl mx-auto p-2'>
-    <Navbar className='border-b-2'></Navbar>
+    
     <Link to='/'  className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
      
      <span className='px-2 py-1 bg-gradient-to-r from-red-900 via-red-700 via-red-500 to-red-400 rounded-lg text-white'>
@@ -28,19 +29,27 @@ function Header() {
      
      </Link>   
     <form className='bg-slate-100 p-1 rounded-lg flex items-center'>
-        <input type='text' placeholder='Search...' 
+        <TextInput placeholder='Search...' 
         className='bg-transparent focus:outline-none'/> 
-        <AiOutlineSearch />
+        <AiOutlineSearch className='text-slate-500' />
     </form>
+    <Button.Group>
     <ul className='flex gap-4'>
-        
+    <Button color="gray">
         <Link to='/'>
-            <li className='hover:underline'> home </li>
+        <HiCloudDownload className="mr-9 h-3 w-4" />
+            <li className='hover:underline'> Home </li>
         </Link> 
+        </Button>
+        <Button color="gray">
         <Link to='/about'>
-            <li className='hover:underline'> about </li>
+        <HiCloudDownload className="mr-9 h-3 w-4" />
+
+            <li className='hover:underline'> About </li>
         </Link>
-        
+        </Button>
+         <Button color="gray">
+         <HiUserCircle className="mr-5 h-8 w-8" />
         <Link to='/profile'>
             {currentUser ? (
               <img
@@ -48,14 +57,15 @@ function Header() {
                 src={currentUser.avatar}
                 alt='profile'
               />
+               
             ) : (
               <li className=' text-slate-700 hover:underline'> Sign in</li>
             )}
           </Link>
-        
+          </Button>
         
     </ul>
-
+</Button.Group>
     </div>
       
     </header>
--- test1/src/Components/pages/About/About.js
+++ test1/src/Components/pages/About/About.js
@@ -1,37 +1,84 @@
-import React from 'react'
-
+import React from 'react';
+import { Accordion } from 'flowbite-react';
 
 export default function About() {
   return (
-    
-    <div className='py-20 px-4 max-w-6xl mx-auto'>
-      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About holberton libery</h1>
-      <p className='mb-4 text-slate-700'>Welcome to <mark>Holberton libery</mark>, your go-to online destination for a curated collection of computer science and programming books.
-       Explore our extensive library designed to fuel your passion for technology and coding.</p>
-      <p className='mb-4 text-slate-700'>
-      Why Choose <mark>Holberton libery?</mark>
-      </p>
-    
-     
-    
-  <br/><hr/>
-
-      <p className='mb-4 text-slate-700'>
-
-<b>Holberton Student Approved:</b> <br/>
-    As a student at Holberton, you can trust our library to complement your Holberton education with additional resources that align with your coursework and help you excel in your studies.
-
-<br/><br/><b>Exemplary Selection: </b> <br/>Dive into our rich virtual library featuring high-quality books covering diverse topics from web development to cybersecurity, data science, and more.
-
-<br/><br/><b>Books for All Levels:</b> <br/> Whether you're a beginner, intermediate, or expert, our library offers books tailored to every experience level. Choose from a variety of titles to match your expertise and specific interests.
-
-<br/><br/><b>Multi-format Options:</b> <br/> Whether you prefer eBooks, audiobooks, or traditional print, we provide multi-format options to suit your personal preferences.
-
-At Holbertonlibery, we believe in providing access to quality computer science education through exceptional resources. Explore our online library today and bring your coding aspirations to life with our inspiring books.
-
-      </p>
+    <div>
+      <div className='py-20 px-4 max-w-6xl mx-auto'>
+        <Accordion collapseAll>
+          <Accordion.Panel>
+            <Accordion.Title>
+              <h1 className='text-3xl font-bold mb-4 text-slate-800'>About holberton libery</h1>
+            </Accordion.Title>
+            <Accordion.Content>
+              <p className='mb-4 text-slate-700'>
+                Welcome to <mark>Holberton libery</mark>, your go-to online destination for a curated collection of computer science and programming books. Explore our extensive library designed to fuel your passion for technology and coding.
+              </p>
+              <p className='mb-4 text-slate-700'>
+                Why Choose <mark>Holberton libery?</mark>
+              </p>
+            </Accordion.Content>
+          </Accordion.Panel>
+          <Accordion.Panel>
+            <Accordion.Title>
+              <h2 className='text-2xl font-bold mb-2'>Holberton Student Approved:</h2>
+            </Accordion.Title>
+            <Accordion.Content>
+              <p className='mb-4 text-slate-700'>
+                As a student at Holberton, you can trust our library to complement your Holberton education with additional resources that align with your coursework and help you excel in your studies.
+              </p>
+            </Accordion.Content>
+          </Accordion.Panel>
+          <Accordion.Panel>
+            <Accordion.Title>
+              <h2 className='text-2xl font-bold mb-2'>Exemplary Selection:</h2>
+            </Accordion.Title>
+            <Accordion.Content>
+              <p className='mb-4 text-slate-700'>
+                Dive into our rich virtual library featuring high-quality books covering diverse topics from web development to cybersecurity, data science, and more.
+              </p>
+            </Accordion.Content>
+          </Accordion.Panel>
+          <Accordion.Panel>
+            <Accordion.Title>
+              <h2 className='text-2xl font-bold mb-2'>Books for All Levels:</h2>
+            </Accordion.Title>
+            <Accordion.Content>
+              <p className='mb-4 text-slate-700'>
+                Whether you're a beginner, intermediate, or expert, our library offers books tailored to every experience level. Choose from a variety of titles to match your expertise and specific interests.
+              </p>
+            </Accordion.Content>
+          </Accordion.Panel>
+          <Accordion.Panel>
+            <Accordion.Title>
+              <h2 className='text-2xl font-bold mb-2'>Multi-format Options:</h2>
+            </Accordion.Title>
+            <Accordion.Content>
+              <p className='mb-4 text-slate-700'>
+                Whether you prefer eBooks, audiobooks, or traditional print, we provide multi-format options to suit your personal preferences.
+              </p>
+            </Accordion.Content>
+          </Accordion.Panel>
+        </Accordion>
       </div>
-      
-  )
-}
 
+      
+
+      <div className='py-20 px-4 max-w-6xl mx-auto'>
+        <Accordion collapseAll>
+          <Accordion.Panel>
+            <Accordion.Title>
+              <h1 className='text-3xl font-bold mb-4 text-slate-800'>about Holberton Search engine</h1>
+            </Accordion.Title>
+            <Accordion.Content>
+              <p className='mb-4 text-slate-700'>
+                Welcome to <mark>Holberton Search engine </mark>This is a demo project
+              </p>
+              
+            </Accordion.Content>
+          </Accordion.Panel>
+        </Accordion>
+      </div>
+    </div>
+  );
+}
