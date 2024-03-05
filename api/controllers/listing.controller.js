be6e1c0e19bd57a55c1fb41460fb8f7ef9784d53
ghayth31 <gharsallighaith31@gmail.com>
2024-03-05 11:35:51 +0100

    create update document api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from '../models/listing.model.js';
+import { errorHandler } from '../utils/eroor.js';
 
 export const createListing = async (req, res, next) => {
   try {
@@ -35,6 +36,26 @@ export const getListing = async (req, res, next) => {
     try {
       await Listing.findByIdAndDelete(req.params.id);
       res.status(200).json('document has been deleted!');
+    } catch (error) {
+      next(error);
+    }
+  };
+  export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, 'document not found!'));
+    }
+   // if (req.user.id !== listing.userRef) {
+      //return next(errorHandler(401, 'You can only update your own document!'));
+   // }
+  
+    try {
+      const updatedListing = await Listing.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+      );
+      res.status(200).json(updatedListing);
     } catch (error) {
       next(error);
     }
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import { createListing, getListing,deleteListing} from '../controllers/listing.controller.js';
-import { verifyToken } from '../utils/verifyUser.js';
-import multer from 'multer';
+import { createListing, getListing,deleteListing, updateListing} from '../controllers/listing.controller.js';
+//import { verifyToken } from '../utils/verifyUser.js';
+//import multer from 'multer';
 
 
 const router = express.Router();
@@ -11,7 +11,7 @@ const router = express.Router();
 
 router.post('/create',  createListing);
 router.delete('/delete/:id',  deleteListing); //router.delete('/delete/:id', verifyToken, deleteListing);
-//router.post('/update/:id', verifyToken, updateListing);
+router.post('/update/:id', updateListing); //router.post('/update/:id', verifyToken, updateListing);
 //router.get('/get/:id', getListing);
 
 export default router;
